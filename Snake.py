b26227c9ad675ed904d6b0c45cd0333350b3347b
Al <afsm1995@live.com>
2019-10-19 10:35:40 -0600

    Game scale is now customizable.

--- Snake.py
+++ Snake.py
@@ -1,6 +1,4 @@
 # ------------------------------------------------ To-Do ------------------------------------------------
-# Make game scale customizable ie 10 pixels size snake vs 1 pixel size.
-# Custom snake size ie 9 block snake vs 5 block snake.
 # Stop food from spawning on the snake.
 # Fix pause
 # Add sound
@@ -22,10 +20,10 @@ import sys
 from random import randrange
 
 # Python snake game.
-def snake(snakeBlockSize, screenWidthInBlockUnits, screenHeighInBlockUnits, snakeSpeed):
+def snake(snakeBlockLength, gameblockScale, screenWidthInBlockUnits, screenHeighInBlockUnits, snakeSpeed):
     # Set up the game window.
-    screenWidthInPixelUnits = screenWidthInBlockUnits * 10
-    screenHeighInPixelUnits = screenHeighInBlockUnits * 10
+    screenWidthInPixelUnits = screenWidthInBlockUnits * gameblockScale
+    screenHeighInPixelUnits = screenHeighInBlockUnits * gameblockScale
     screen = game.display.set_mode((screenWidthInPixelUnits, screenHeighInPixelUnits))
     game.display.set_caption(' Snake')
     game.display.set_icon(game.image.load(r'C:\Users\Alvaro Santillan\Documents\Programming Projects\perfect_snake\logo.png').convert())
@@ -33,14 +31,14 @@ def snake(snakeBlockSize, screenWidthInBlockUnits, screenHeighInBlockUnits, snak
     # Create and position the snake and the food.
     # Set the snake head position at bottom of screen x pixels before the screen ends.
     snakeDirection = 'right'
-    snakeBlockSize = (snakeBlockSize*10)-10
-    snakeYPos = screenHeighInPixelUnits-10
+    snakeBlockLength = (snakeBlockLength*gameblockScale)-gameblockScale
+    snakeYPos = screenHeighInPixelUnits-gameblockScale
     snakeBodyPos = []
 
-    for i in range(snakeBlockSize, -10, -10):
+    for i in range(snakeBlockLength, -gameblockScale, -gameblockScale):
         snakeBodyPos.append([i,snakeYPos])
     snakeBodyPos.insert(0, list(snakeBodyPos[0]))
-    foodPos = [randrange(0,screenWidthInPixelUnits, 10),randrange(0,screenHeighInPixelUnits, 10)]
+    foodPos = [randrange(0,screenWidthInPixelUnits, gameblockScale),randrange(0,screenHeighInPixelUnits, gameblockScale)]
 
     # Set up the game colors.
     white = game.Color(255, 255, 255)
@@ -73,13 +71,13 @@ def snake(snakeBlockSize, screenWidthInBlockUnits, screenHeighInBlockUnits, snak
         if not paused:
             # Create new block at snake head according to last key pressed.
             if snakeDirection == 'right':
-                snakeBodyPos[0][0] += 10
+                snakeBodyPos[0][0] += gameblockScale
             elif snakeDirection == 'left':
-                snakeBodyPos[0][0] -= 10
+                snakeBodyPos[0][0] -= gameblockScale
             elif snakeDirection == 'up':
-                snakeBodyPos[0][1] -= 10
+                snakeBodyPos[0][1] -= gameblockScale
             else:
-                snakeBodyPos[0][1] += 10
+                snakeBodyPos[0][1] += gameblockScale
 
             # Insert a new block at snake head according to last key pressed.    
             snakeBodyPos.insert(0, list(snakeBodyPos[0]))
@@ -91,23 +89,24 @@ def snake(snakeBlockSize, screenWidthInBlockUnits, screenHeighInBlockUnits, snak
             # Handle if snake head touched the food.
             elif snakeBodyPos[0] == foodPos:
                 gameScore += 1
-                foodPos = [randrange(0,screenWidthInPixelUnits, 10),randrange(0,screenHeighInPixelUnits, 10)]
+                foodPos = [randrange(0,screenWidthInPixelUnits, gameblockScale),randrange(0,screenHeighInPixelUnits, gameblockScale)]
             # Pop and hide last tail block to simulate movement.
             else:
-                game.draw.rect(screen, black, game.Rect(snakeBodyPos[-1][0],snakeBodyPos[-1][1],9,9))
+                game.draw.rect(screen, black, game.Rect(snakeBodyPos[-1][0],snakeBodyPos[-1][1],gameblockScale,gameblockScale))
                 snakeBodyPos.pop()
 
             # Check for a self collision and draw the snake.
             for index, bodyBlock in enumerate(snakeBodyPos):
-                game.draw.rect(screen, white, game.Rect(bodyBlock[0],bodyBlock[1],9,9))
+                game.draw.rect(screen, white, game.Rect(bodyBlock[0],bodyBlock[1],gameblockScale,gameblockScale))
                 if index > 4 and snakeBodyPos[0] == bodyBlock:
                     gameOver(gameScore)
 
             # Draw food and grid.
-            game.draw.rect(screen, white, game.Rect(foodPos[0],foodPos[1],9,9))
-            for i in range(-1, screenWidthInPixelUnits, 10):
-                game.draw.line(screen, lightGray, [i, screenHeighInPixelUnits], [i,0], 1)
-                game.draw.line(screen, lightGray, [screenWidthInPixelUnits, i], [0,i], 1)
+            game.draw.rect(screen, white, game.Rect(foodPos[0],foodPos[1],gameblockScale,gameblockScale))
+            if gameblockScale != 1:
+                for i in range(-1, screenWidthInPixelUnits, gameblockScale):
+                    game.draw.line(screen, lightGray, [i, screenHeighInPixelUnits], [i,0], 1)
+                    game.draw.line(screen, lightGray, [screenWidthInPixelUnits, i], [0,i], 1)
             
             # Load new frame on the screen and set famerate. 
             game.display.update()
@@ -120,5 +119,6 @@ def gameOver(gameScore):
     print("Score:", gameScore)
     print("Game Length (ms):", game.time.get_ticks())
 
-# snakeBlockSize, screen width in block units, screen height in block units, game speed
-snake(9,30,30,2)
+# snakeBlockLength, gameblockScale, screenWidthInBlockUnits, screenHeighInBlockUnits, snakeSpeed
+# Note: Snake length must be between 1 and (screen width -1). 
+snake(9,10,30,30,10)
